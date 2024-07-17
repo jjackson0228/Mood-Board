6d9d35bd6bc7983de4675563c766e0eca4e891ed
James Jackson <jamesjackson@Jamess-Air.lan>
2024-07-17 15:42:50 -0400

    added all css for mood board and control pannel

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,24 @@ clearBtn.addEventListener("click", function () {
   localStorage.clear();
   window.location.reload();
 });
+// ? Function to update local storage. This function will be called after a new element is added to the mood board and the tempStorageObject is updated.
+
+function updateLocalStorage() {
+  localStorage.setItem("moodBoardData", JSON.stringify(tempStorageObject));
+}
+// ? Function to load from local storage. This function will be called on page load.
+
+function loadFromLocalStorage() {
+  const storedData = JSON.parse(localStorage.getItem("moodBoardData"));
+  if (storedData) {
+    tempStorageObject = storedData;
+  }
+}
+tempStorageObject.text.forEach((text) => {
+  const img = document.createElement("img");
+  img.src = image.url;
+  img.style.left = image.left;
+  img.style.top = image.top;
+  img.classList.add("draggable");
+  moodBoardEl.appendChild(img);
+});
